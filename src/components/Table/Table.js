be6592ba9dc69544d26e82d1674c45d4d06d4f2c
shagundeep <shagundeep17@gmail.com>
2019-12-02 00:23:16 +0530

    add realtime dashboard

---
+++ src/components/Table/Table.js
@@ -0,0 +1,78 @@
+import React from "react";
+import PropTypes from "prop-types";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+// core components
+import styles from "assets/jss/material-dashboard-react/components/tableStyle.js";
+
+const useStyles = makeStyles(styles);
+var count=0;
+export default function CustomTable(props) {
+  const classes = useStyles();
+  const { tableHead, tableData, tableHeaderColor, clickable} = props;
+  return (
+    <div className={classes.tableResponsive}>
+      <Table className={classes.table}>
+        {tableHead !== undefined ? (
+          <TableHead className={classes[tableHeaderColor + "TableHeader"]}>
+            <TableRow className={classes.tableHeadRow}>
+              {tableHead.map((prop, key) => {
+                return (
+                  <TableCell
+                    className={classes.tableCell + " " + classes.tableHeadCell}
+                    key={key}
+                  >
+                    {prop}
+                  </TableCell>
+                );
+              })}
+            </TableRow>
+          </TableHead>
+        ) : null}
+        <TableBody>
+          {tableData.map((prop, key) => {
+            return (
+              <TableRow key={key} className={classes.tableBodyRow}>
+                {prop.map((prop, key) => {
+                  count=(count+1)%3
+                  return (
+                    <TableCell className={classes.tableCell} key={key} >
+                     {clickable && count===0 && <a href={prop} >  {prop}</a>}
+                     {clickable&& count!==0 && prop}
+                     {!clickable && prop}
+                    </TableCell>
+                  );
+                })}
+              </TableRow>
+            );
+          },
+          
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
+
+CustomTable.defaultProps = {
+  tableHeaderColor: "gray"
+};
+
+CustomTable.propTypes = {
+  tableHeaderColor: PropTypes.oneOf([
+    "warning",
+    "primary",
+    "danger",
+    "success",
+    "info",
+    "rose",
+    "gray"
+  ]),
+  tableHead: PropTypes.arrayOf(PropTypes.string),
+  tableData: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.string))
+};
---
+++ src/views/Dashboard/Dashboard.js
@@ -0,0 +1,363 @@
+import React from "react";
+// react plugin for creating charts
+import ChartistGraph from "react-chartist";
+// @material-ui/core
+import { makeStyles } from "@material-ui/core/styles";
+import {Polar,Pie} from "react-chartjs-2";
+import GoogleMapReact from 'google-map-react';
+import Icon from "@material-ui/core/Icon";
+//import { Radar } from "react-chartjs-2";
+// @material-ui/icons
+import Store from "@material-ui/icons/Store";
+import Warning from "@material-ui/icons/Warning";
+import DateRange from "@material-ui/icons/DateRange";
+import LocalOffer from "@material-ui/icons/LocalOffer";
+import Update from "@material-ui/icons/Update";
+import ArrowUpward from "@material-ui/icons/ArrowUpward";
+import AccessTime from "@material-ui/icons/AccessTime";
+import Accessibility from "@material-ui/icons/Accessibility";
+import BugReport from "@material-ui/icons/BugReport";
+import Code from "@material-ui/icons/Code";
+import Cloud from "@material-ui/icons/Cloud";
+// core components
+import GridItem from "components/Grid/GridItem.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import Table from "components/Table/Table.js";
+import Tasks from "components/Tasks/Tasks.js";
+import CustomTabs from "components/CustomTabs/CustomTabs.js";
+import Danger from "components/Typography/Danger.js";
+import Card from "components/Card/Card.js";
+import CardHeader from "components/Card/CardHeader.js";
+import CardIcon from "components/Card/CardIcon.js";
+import CardBody from "components/Card/CardBody.js";
+import CardFooter from "components/Card/CardFooter.js";
+import PieChart from 'react-minimal-pie-chart';
+import { bugs, website, server } from "variables/general.js";
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import {
+  dailySalesChart,
+  emailsSubscriptionChart,
+  completedTasksChart
+} from "variables/charts.js";
+
+import styles from "assets/jss/material-dashboard-react/views/dashboardStyle.js";
+
+const useStyles = makeStyles(styles);
+const options = {
+  exportEnabled: true,
+  animationEnabled: true,
+  title: {
+    text: "Website Traffic Sources"
+  },
+  data: [{
+    type: "pie",
+    startAngle: 75,
+    toolTipContent: "<b>{label}</b>: {y}%",
+    showInLegend: "true",
+    legendText: "{label}",
+    indexLabelFontSize: 16,
+    indexLabel: "{label} - {y}%",
+    dataPoints: [
+      { y: 18, label: "Direct" },
+      { y: 49, label: "Organic Search" },
+      { y: 9, label: "Paid Search" },
+      { y: 5, label: "Referral" },
+      { y: 19, label: "Social" }
+    ]
+  }]
+};
+
+const mapStyles = {
+  width: '100%',
+  height: '100%',
+};
+
+const dataPie = {
+  labels: ["Haha", "Angry", "Like", "Love", "Wow"],
+  datasets: [
+    {
+      data: [300, 50, 100, 40, 120],
+      backgroundColor: [
+        "#F7464A",
+        "#46BFBD",
+        "#FDB45C",
+        "#949FB1",
+        "#4D5360",
+        "#ac64ad"
+      ],
+      hoverBackgroundColor: [
+        "#FF5A5E",
+        "#5AD3D1",
+        "#FFC870",
+        "#A8B3C5",
+        "#616774",
+        "#da92db"
+      ]
+    }
+  ]
+};
+
+const dataPolar = {
+  datasets: [
+    {
+      data: [11, 16, 7, 3, 14,3,45,4,12,23,12,32],
+      backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360",'#FB9E00', '#808900', '#194D33', '#0C797D', '#0062B1', '#653294', '#AB149E'],
+      label: "My dataset" // for legend
+    }
+  ],
+  labels: ["Jan", "Feb", "Mar", "Apr", "May","June","July","Aug","Sept","Oct","Nov","Dec"]
+};
+  
+const defaultProps = {
+  center: {
+    lat: 59.95,
+    lng: 30.33
+  },
+  zoom: 11
+};
+  
+
+export default function Dashboard() {
+  const classes = useStyles();
+  return (
+    <div>
+      <GridContainer>
+        <GridItem xs={12} sm={6} md={3}>
+          <Card>
+            <CardHeader color="warning" stats icon>
+            <CardIcon color="info">
+            <Accessibility />
+              </CardIcon>
+              <p className={classes.cardCategory}>Number of active users</p>
+              <h3 className={classes.cardTitle}>
+                78/100 
+              </h3>
+            </CardHeader>
+            <CardFooter stats>
+              <div className={classes.stats}>
+                <Danger>
+                  <Warning />
+                </Danger>
+                <a href="#pablo" onClick={e => e.preventDefault()}>
+                  Get more space
+                </a>
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={6} md={3}>
+          <Card>
+            <CardHeader color="success" stats icon>
+            <CardIcon color="warning">
+                <Icon>content_copy</Icon>
+              </CardIcon>
+              <p className={classes.cardCategory}>Number of terror related posts</p>
+              <h3 className={classes.cardTitle}>60</h3>
+            </CardHeader>
+            <CardFooter stats>
+              <div className={classes.stats}>
+                <DateRange />
+                Last 24 Hours
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={6} md={3}>
+          <Card>
+            <CardHeader color="danger" stats icon>
+              <CardIcon color="danger">
+                <Icon>info_outline</Icon>
+              </CardIcon>
+              <p className={classes.cardCategory}>Fixed Issues</p>
+              <h3 className={classes.cardTitle}>75</h3>
+            </CardHeader>
+            <CardFooter stats>
+              <div className={classes.stats}>
+                <LocalOffer />
+                Tracked from Github
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={6} md={3}>
+          <Card>
+            <CardHeader color="info" stats icon>
+              <CardIcon color="info">
+                <Accessibility />
+              </CardIcon>
+              <p className={classes.cardCategory}>Followers</p>
+              <h3 className={classes.cardTitle}>+245</h3>
+            </CardHeader>
+            <CardFooter stats>
+              <div className={classes.stats}>
+                <Update />
+                Just Updated
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
+      </GridContainer>
+      <GridContainer>
+        {/* <GridItem xs={12} sm={12} md={4}>
+          {/* <Card chart>
+            {/* <CardHeader color="success">
+              <ChartistGraph
+                className="ct-chart"
+                data={dailySalesChart.data}
+                type="Line"
+                options={dailySalesChart.options}
+                listener={dailySalesChart.animation}
+              />
+            </CardHeader> */}
+            {/* <CardBody>
+              <h4 className={classes.cardTitle}>Daily Sales</h4>
+              <p className={classes.cardCategory}>
+                <span className={classes.successText}>
+                  <ArrowUpward className={classes.upArrowCardCategory} /> 55%
+                </span>{" "}
+                increase in today sales.
+              </p>
+            </CardBody> */}
+            {/* <CardFooter chart>
+              <div className={classes.stats}>
+                <AccessTime /> updated 4 minutes ago
+              </div>
+            </CardFooter>
+          </Card> */}
+        {/* //</GridItem>  */}
+        
+        <GridItem xs={12} sm={12} md={6}>
+          <Card chart>
+            <CardHeader color="warning">
+              <ChartistGraph
+                className="ct-chart"
+                data={emailsSubscriptionChart.data}
+                type="Bar"
+                options={emailsSubscriptionChart.options}
+                responsiveOptions={emailsSubscriptionChart.responsiveOptions}
+                listener={emailsSubscriptionChart.animation}
+              />
+            </CardHeader>
+            <CardBody>
+              <h4 className={classes.cardTitle}>Active users</h4>
+              <p className={classes.cardCategory}>Users reactring most on terror related posts</p>
+            </CardBody>
+            <CardFooter chart>
+              <div className={classes.stats}>
+                <AccessTime /> campaign sent 2 days ago
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={12} md={6}>
+        <Card chart>
+            <CardHeader >
+            <Pie data={dataPie} options={{ responsive: true }} />
+            {/* <PieChart className=""
+            data={[
+            { title: 'hhh', value: 10, color: '#E38627', label: "Direct"},
+            { title: 'jjjj', value: 15, color: '#C13C37',label: "Direct1" },
+            { title: 'kkkk', value: 20, color: '#6A2135',label: "Direct2" },
+            ]}
+            label={true}
+            />; */}
+            </CardHeader>
+            <CardBody>
+              <h4 className={classes.cardTitle}>Reactions on posts</h4>
+              <p className={classes.cardCategory}>Last 1 month</p>
+            </CardBody>
+          </Card>
+        </GridItem>
+        <div style={{ height: '40vh', width: '30%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 1234 }}
+          defaultCenter={{lat: 59.95,
+            lng: 30.33}}
+          defaultZoom={11}
+        >
+          <div
+            lat={59.955413}
+            lng={30.337844}
+            text="My Marker"
+          />
+        </GoogleMapReact>
+      </div>
+      <GridItem xs={8} sm={8} md={8}>
+      <Card>
+      <CardHeader color="warning" stats icon>
+      <Polar data={dataPolar} options={{ responsive: true }} />
+      </CardHeader>
+      <CardBody>
+              <h4 className={classes.cardTitle}> Number of active users monthly statement</h4>
+            </CardBody>
+            </Card>
+      </GridItem>
+      </GridContainer>
+      {/* <GridContainer>
+        <GridItem xs={12} sm={12} md={6}>
+          <CustomTabs
+            title="Tasks:"
+            headerColor="primary"
+            tabs={[
+              {
+                tabName: "Bugs",
+                tabIcon: BugReport,
+                tabContent: (
+                  <Tasks
+                    checkedIndexes={[0, 3]}
+                    tasksIndexes={[0, 1, 2, 3]}
+                    tasks={bugs}
+                  />
+                )
+              },
+              {
+                tabName: "Website",
+                tabIcon: Code,
+                tabContent: (
+                  <Tasks
+                    checkedIndexes={[0]}
+                    tasksIndexes={[0, 1]}
+                    tasks={website}
+                  />
+                )
+              },
+              {
+                tabName: "Server",
+                tabIcon: Cloud,
+                tabContent: (
+                  <Tasks
+                    checkedIndexes={[1]}
+                    tasksIndexes={[0, 1, 2]}
+                    tasks={server}
+                  />
+                )
+              }
+            ]}
+          />
+        </GridItem>
+        <GridItem xs={12} sm={12} md={6}>
+          <Card>
+            <CardHeader color="warning">
+              <h4 className={classes.cardTitleWhite}>Employees Stats</h4>
+              <p className={classes.cardCategoryWhite}>
+                New employees on 15th September, 2016
+              </p>
+            </CardHeader>
+            <CardBody>
+              <Table
+                tableHeaderColor="warning"
+                tableHead={["ID", "Name", "Salary", "Country"]}
+                tableData={[
+                  ["1", "Dakota Rice", "$36,738", "Niger"],
+                  ["2", "Minerva Hooper", "$23,789", "Curaçao"],
+                  ["3", "Sage Rodriguez", "$56,142", "Netherlands"],
+                  ["4", "Philip Chaney", "$38,735", "Korea, South"]
+                ]}
+              />
+            </CardBody>
+          </Card>
+        </GridItem>
+      </GridContainer> */}
+    </div>
+  );
+}
